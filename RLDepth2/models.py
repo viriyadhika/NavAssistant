1d8e58bbc91ffc49cdd808d6402eb105b86d0ff8
viriyadhika <viriya.dhika0@gmail.com>
2025-11-20 22:53:43 -0500

    make bigger

--- RLDepth2/models.py
+++ RLDepth2/models.py
@@ -136,7 +136,7 @@ class ActorCritic(nn.Module):
     ):
         super().__init__()
         self.encoder = FusedEncoder(feat_dim, device)
-        self.gru = nn.GRU(input_size=feat_dim, num_layers=8, hidden_size=hidden_dim, batch_first=True)
+        self.gru = nn.GRU(input_size=feat_dim, num_layers=16, hidden_size=hidden_dim, batch_first=True)
 
         self.policy_head = nn.Linear(hidden_dim, num_actions)
         self.value_head = nn.Linear(hidden_dim, 1)
